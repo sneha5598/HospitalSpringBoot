2275400aa567a5aa7c8666b1973653f7da27b48d
Sneha5598 <raosneha5598@gmail.com>
2021-03-29 20:33:38 +0530

    first commit

---
+++ Logg/src/log/ex_log.java
@@ -0,0 +1,16 @@
+package log;
+import java.util.logging.*;
+//import java.util.*;
+
+
+public class ex_log {
+	static Logger l = Logger.getLogger(ex_log.class.getName());  
+	public static void main(String[] args) {
+		//l.log(Level.INFO,"sense");
+		l.info("hii");
+	
+		
+
+	}
+
+}
